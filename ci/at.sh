eeec1b660e0622e082eca08c56a23fced530d4db
by46 <ycs_ctbu_2010@126.com>
2016-08-16 14:43:35 +0800

    add flask-log

--- ci/at.sh
+++ ci/at.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-echo AT
+IMAGE=$(sirius docker_image_name | head -n 1)
+
+sirius docker_deploy:meerkat,${IMAGE},server=scdfis01,ports="3141;8080",env="ENV\=gqc"
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-IMAGE=$(sirius docker_image_name | head -n 1)
 
-sirius docker_deploy:meerkat,${IMAGE},server=scmesos06,ports="3141;8080",env="ENV\=prd"
