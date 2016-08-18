db946ae570a3814544b7f27a11b37fc6713df804
by46 <ycs_ctbu_2010@126.com>
2016-08-18 10:57:49 +0800

    format meerkat home

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
+IMAGE=$(sirius docker_image_name | head -n 1)
 
+# build release image
+
+RELEASE_IMAGE=$(sirius docker_image_name:release=true | head -n 1)
+
+docker tag -f ${IMAGE} docker.neg/${RELEASE_IMAGE}
+
+docker push docker.neg/${RELEASE_IMAGE}
+
+[ $? -gt 0 ] && exit 2
+
+exit 0
